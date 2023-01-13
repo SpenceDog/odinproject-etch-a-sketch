6719e769c76a85111787b7d8c249174623b15b7e
SpenceDog <git@spencerlynch.us>
2023-01-12 22:53:52 -0500

    Added Reset Button.

--- etch.js
+++ etch.js
@@ -1,15 +1,36 @@
 // This is the code that is used to create the etch a sketch canvas.
 function etch(){
-    var boxWidth = 16; // Box dimensions.
-    var boxHeight = 16;
 
-    for (let i = 0; i < boxHeight; i++) { // For each row...
+    var gridWidth = 1024; // Sets our total etch size.
+    var gridHeight = 1024;
+
+    var userInputWidth; // Declars size prompt.
+    var userInputHeight;
+
+    while(!/^[0-9][0-9]+$/.test(userInputWidth)){ // While it is NOT a number between 1-100...
+        userInputWidth = prompt("Please enter a width between 10-100"); // Get Prompts.
+        userInputWidth--; // Not optimal, but easy loophole to regex that is decently safe for this project.
+    }
+
+    userInputWidth++;
+
+    while(!/^[0-9][0-9]+$/.test(userInputHeight)){
+        userInputHeight = prompt("Please enter a height between 10-100");
+        userInputHeight--
+    }
+
+    userInputHeight++;
+
+    var boxWidth = gridWidth/userInputWidth; // Box dimensions.
+    var boxHeight = gridHeight/userInputHeight;
+
+    for (let i = 0; i < userInputHeight; i++) { // For each row...
         const rowContent = document.createElement("div"); // Creates a div.
        rowContent.id = "etch-row-"+i;
        rowContent.className = "etch-row"
       document.getElementById("etch-box-content").appendChild(rowContent);
 
-    for(let y = 0; y < boxWidth; y++){ // For each column in a row...
+    for(let y = 0; y < userInputWidth; y++){ // For each column in a row...
         const colContent = document.createElement("div"); // Creates a cell.
         colContent.id = "etch-col-"+i+"-"+y;
         colContent.className = "etch-col"
